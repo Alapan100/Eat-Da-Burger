07dff138345a75f742cd6e1be3d9c316c12699f6
Alapan Bhakat <alapanbhakat@gmail.com>
2019-06-24 15:15:33 -0700

    updated rugurgitate back

--- controllers/burgerController.js
+++ controllers/burgerController.js
@@ -2,32 +2,32 @@ var express = require('express');
 var router = express.Router();
 var burger = require('../models/burgers.js');
 
-router.get('/', function(req,res) {
+router.get('/', function (req, res) {
 	res.redirect('/burgers')
 });
 
-router.get('/burgers', function(req,res) {
-	burger.all(function(data){
-		var hbsObject = {burgers : data}
-		console.log(hbsObject)
+router.get('/burgers', function (req, res) {
+	burger.all(function (data) {
+		var hbsObject = { burgers: data }
+		// console.log(hbsObject)
 		res.render('index', hbsObject);
 	});
 });
 
-router.post('/burgers/create', function(req,res) {
-	burger.create(['burger_name', 'devoured'], [req.body.burger_name, req.body.devoured], function(data){
+router.post('/burgers/create', function (req, res) {
+	burger.create(['burger_name', 'devoured'], [req.body.burger_name, req.body.devoured], function (data) {
 		res.redirect('/burgers')
 	});
 });
 
-router.put('/burgers/update/:id', function(req,res) {
+router.put('/burgers/update/:id', function (req, res) {
 	var condition = 'id = ' + req.params.id;
-
-	console.log('condition', condition);
-
-	burger.update({'devoured' : req.body.devoured}, condition, function(data){
+	console.log(req.params)
+	// console.log('condition', condition);
+	burger.update({ 'devoured': req.body.devoured }, condition, function (data) {
 		res.redirect('/burgers');
 	});
+	
 });
 
 module.exports = router;
